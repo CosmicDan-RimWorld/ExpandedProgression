ca5e2275467dbbb1550e4ef62446d97e9142cf77
CosmicDan <dconnolly.au@gmail.com>
2017-06-28 14:52:07 +1000

    [Native] Patch Carbon fuel to be 3x as valuable

---
+++ Mods/ExpandedProgression/Source/ExpandedProgression/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Harmony;
+using Verse;
+using System.Reflection;
+
+namespace ExpandedProgressionNative
+{
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static Main()
+        {
+            // this constructor is just used to signal Harmony to look through this assembly for all patches
+            var harmony = HarmonyInstance.Create("com.cosmicdan.rimworld.expandedprogression");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Log.Message("ExpandedProgression mod native has loaded!");
+        }
+    }
+
+    public class Util
+    {
+        internal static bool isCarbonThing(Thing thing)
+        {
+            foreach (var thingCategory in thing.def.thingCategories)
+                if (thingCategory.defName.Equals("Carbon"))
+                    return true;
+
+            return false;
+        }
+    }
+
+}
---
+++ Mods/ExpandedProgression/Source/ExpandedProgression/Patches/GetFuelPatch.cs
@@ -0,0 +1,36 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using Verse;
+using Verse.AI;
+
+namespace ExpandedProgressionNative.Patches
+{
+    // hook the job for getting fuel - if carbon item is designated, divide the count (for delivery) by 3
+    [HarmonyPatch(typeof(WorkGiver_Refuel))]
+    [HarmonyPatch("JobOnThing")]
+    [HarmonyPatch(new Type[] { typeof(Pawn), typeof(Thing), typeof(bool) })]
+    class WorkGiver_Refuel__JobOnThing
+    {
+        static void Postfix(WorkGiver_Refuel __instance, ref Job __result)
+        {
+            if (Util.isCarbonThing(__result.targetB.Thing))
+                __result.count /= 3;
+        }
+
+    }
+
+    // hook the actual fuel insertion action - if carbon item is added, multiply it by 3
+    [HarmonyPatch(typeof(CompRefuelable))]
+    [HarmonyPatch("Refuel")]
+    [HarmonyPatch(new Type[] { typeof(Thing) })]
+    class PatchCompRefuelable__Refuel
+    {
+        static void Prefix(CompRefuelable __instance, ref Thing fuelThing)
+        {
+            if (Util.isCarbonThing(fuelThing))
+                fuelThing.stackCount *= 3;
+        }
+
+    }
+}
